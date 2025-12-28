680461a4488e0a82d5caf1d4b0c08ed48938476d
Arkadiusz Moscicki <arkadiuszmoscicki@gmail.com>
2025-12-28 20:21:15 +0000

    fix: rabbitai comment

--- src/client.tsx
+++ src/client.tsx
@@ -1,13 +1,10 @@
 import { StartClient } from "@tanstack/react-start/client";
 import { StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
-import { NuqsAdapter } from "nuqs/adapters/react";
 
 hydrateRoot(
   document,
   <StrictMode>
-    <NuqsAdapter>
-      <StartClient />
-    </NuqsAdapter>
+    <StartClient />
   </StrictMode>
 );
